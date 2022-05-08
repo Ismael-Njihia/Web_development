db96a5d5307c7cf64a568010278edd3c35211099
Ishmael <100527231+EngIshmael@users.noreply.github.com>
2022-05-08 22:50:40 +0300

    Add files via upload
    Cool  background here!

---
+++ njihia.js
@@ -0,0 +1,19 @@
+function blue() {
+    var x = document.getElementById('bg');
+    x.style.backgroundColor = 'blue'
+}
+
+function red() {
+    var y = document.getElementById('bg');
+    y.style.backgroundColor = 'red'
+}
+
+function Green() {
+    var z = document.getElementById('bg');
+    z.style.backgroundColor = 'green'
+}
+
+function Default() {
+    var a = document.getElementById('bg');
+    a.style.backgroundColor = 'black'
+}
